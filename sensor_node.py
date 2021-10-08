3365cc438a00d98f2cce19941b2616e2b06f21d5
raspi <you@example.com>
2021-10-08 14:49:15 +0900

    fix

--- sensor_node.py
+++ sensor_node.py
@@ -5,10 +5,14 @@ from std_msgs.msg import Float32MultiArray
 import os
 from proximity_sensor import *
 
-os.environ["ROS_MASTER_URI"] = "http://192.168.1.56:11311"
-os.environ["ROS_IP"] = "192.168.1.200"
+os.environ["ROS_MASTER_URI"] = "http://192.168.100.10:11311"
+os.environ["ROS_IP"] = "192.168.100.200"
 
 def main():
+    #s = ProximitySensor( 0x29 )
+    #s.change_address( 0x10 )    
+    #return
+
     rospy.init_node('proximity_sensor')
     
     s1 = ProximitySensor( 0x10 )
